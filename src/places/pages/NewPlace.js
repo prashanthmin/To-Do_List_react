4dca52e11b3f7cb1943d1b94b42e563d3b1db85c
Gaddam Prashanth <prashanthnetha012@gmail.com>
2025-05-31 13:17:30 +0530

    styling components

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,7 @@
 import React from "react";
 const NewPlace = () => {
     return (
-        <h2>this is new places</h2>
+        <h2>this is new places hello </h2>
     );
     };
 export default NewPlace;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import UsersList from '../components/UsersList';
 const Users = () => {
-    return <h2>Hello users!! </h2>;
+    const USER = [{
+        id: 'u1',
+        name: 'Max Schwarz',
+        image: 'https://tse1.mm.bing.net/th?id=OIP.n2PgfD5L-6a0pWo-wcbeVAHaEK&pid=Api&P=0&h=220',
+        places: 3
+    }];
+    return <UsersList items= {USER} />;
 };
 
 export default Users;
